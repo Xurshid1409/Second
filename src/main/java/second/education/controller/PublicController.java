368ce4d4ef0e02c11bc509f13ccc6fd5c5e5d944
shahzod277 <shahzodozbekov7@gmail.com>
2022-07-19 15:36:33 +0500

    dwa

--- src/main/java/second/education/controller/PublicController.java
+++ src/main/java/second/education/controller/PublicController.java
@@ -2,30 +2,36 @@ package second.education.controller;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import second.education.model.response.FutureInstitutionResponse;
+import second.education.model.response.UniversityResponse;
+import second.education.service.FutureInstitutionService;
+import second.education.service.api.DiplomaApi;
 import second.education.service.api.OneIdServiceApi;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/public")
 @RequiredArgsConstructor
-@SecurityRequirement(name = "second")
 public class PublicController {
 
-    private final OneIdServiceApi oneIdServiceApi;
-   // private final EnrolleeService enrolleeService;
+    private final DiplomaApi diplomaApi;
+    private final FutureInstitutionService futureInstitutionService;
 
-/*    @GetMapping("oneId/redirect")
-    public ResponseEntity<?> OneId() {
-        URI uri = oneIdServiceApi.redirectOneIdUrl();
-        return ResponseEntity.status(HttpStatus.FOUND).location(uri).build();
-    }*/
+    @GetMapping("getUniversities")
+    public ResponseEntity<?> getUniversities() {
+        List<UniversityResponse> universities = diplomaApi.getUniversities();
+        return ResponseEntity.status(!universities.isEmpty() ? 200 : 404).body(universities);
+    }
 
-/*    @GetMapping("oneId/signIn/qabul.jsp")
-    public ResponseEntity<?> signIn(@RequestParam(value = "code") String code) {
-        Result result = enrolleeService.signUp(code);
-        return ResponseEntity.status(result.isSuccess() ? 200 : 400).body(result);
-    }*/
-
+    @GetMapping("futureInstitution")
+    public ResponseEntity<?> getAllFutureInstitution() {
+        List<FutureInstitutionResponse> allFutureInstitution = futureInstitutionService.getAllFutureInstitution();
+        return ResponseEntity.ok(allFutureInstitution);
+    }
 
 }
