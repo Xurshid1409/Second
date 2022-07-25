51318bb989ac147992918a6c62e9897e5fac9bf2
shahzod277 <shahzodozbekov7@gmail.com>
2022-07-25 15:29:14 +0500

    dwa

--- src/main/java/second/education/model/response/UAdminInfoResponse.java
+++ src/main/java/second/education/model/response/UAdminInfoResponse.java
@@ -3,6 +3,7 @@ package second.education.model.response;
 import lombok.Getter;
 import lombok.Setter;
 import second.education.domain.AdminEntity;
+import second.education.domain.classificator.University;
 
 @Getter
 @Setter
@@ -19,7 +20,14 @@ public class UAdminInfoResponse {
         this.id = adminEntity.getId();
         this.firstname = adminEntity.getFistName();
         this.lastname = adminEntity.getLastname();
-        this.futureInstitutionId = adminEntity.getFutureInstitution().getId();
-        this.futureInstitutionName = adminEntity.getFutureInstitution().getName();
+        if (adminEntity.getFutureInstitution() != null) {
+            this.futureInstitutionId = adminEntity.getFutureInstitution().getId();
+            this.futureInstitutionName = adminEntity.getFutureInstitution().getName();
+
+        } else {
+            University university = adminEntity.getUniversities().stream().findFirst().get();
+            this.futureInstitutionId = university.getInstitutionId();
+            this.futureInstitutionName = university.getInstitutionName();
+        }
     }
 }
