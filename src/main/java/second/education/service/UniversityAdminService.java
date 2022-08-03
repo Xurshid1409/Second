e770662375b3e825c20ec9905b1e5292f79ba6f5
Legion <hersh1409@gmail.com>
2022-08-03 16:55:37 +0500

    auth service deploying

--- src/main/java/second/education/service/UniversityAdminService.java
+++ src/main/java/second/education/service/UniversityAdminService.java
@@ -301,23 +301,22 @@ public class UniversityAdminService {
         try {
             AdminEntity adminEntity = adminEntityRepository.getAdminUniversity(principal.getName()).get();
             Integer institutionId = adminEntity.getUniversities().stream().map(University::getInstitutionId).findFirst().get();
-
-            Optional<Application> app = applicationRepository.getAppByUadmin(diplomaId, adminEntity.getFutureInstitution().getId());
-            if (app.isPresent()) {
-                app.get().setDiplomaStatus(updateDiplomaStatus.getDiplomStatus());
-                app.get().setDiplomaMessage(updateDiplomaStatus.getDiplomMessage());
-                Application save = applicationRepository.save(app.get());
-                StoryMessage storyMessage = new StoryMessage();
-                storyMessage.setMessage(updateDiplomaStatus.getDiplomMessage());
-                String status1 = String.valueOf(updateDiplomaStatus.getDiplomStatus());
-                storyMessage.setStatus(status1);
-                storyMessage.setFirstname(adminEntity.getFistName());
-                storyMessage.setLastname(adminEntity.getLastname());
-                storyMessage.setPinfl(principal.getName());
-                storyMessage.setApplication(save);
-                storyMessageRepository.save(storyMessage);
-                return new Result("Muvaffaqiyatli o'zgartirildi", true);
-            }
+//            Optional<Application> app = applicationRepository.getAppByUadmin(adminEntity.getFutureInstitution().getId(), diplomaId);
+//            if (app.isPresent()) {
+//                app.get().setDiplomaStatus(updateDiplomaStatus.getDiplomStatus());
+//                app.get().setDiplomaMessage(updateDiplomaStatus.getDiplomMessage());
+//                Application save = applicationRepository.save(app.get());
+//                StoryMessage storyMessage = new StoryMessage();
+//                storyMessage.setMessage(updateDiplomaStatus.getDiplomMessage());
+//                String status1 = String.valueOf(updateDiplomaStatus.getDiplomStatus());
+//                storyMessage.setStatus(status1);
+//                storyMessage.setFirstname(adminEntity.getFistName());
+//                storyMessage.setLastname(adminEntity.getLastname());
+//                storyMessage.setPinfl(principal.getName());
+//                storyMessage.setApplication(save);
+//                storyMessageRepository.save(storyMessage);
+//                return new Result("Muvaffaqiyatli o'zgartirildi", true);
+//            }
             Optional<Application> application = applicationRepository.getAppAndDiplomaById(institutionId, diplomaId);
             if (application.isPresent()) {
                 application.get().setDiplomaStatus(updateDiplomaStatus.getDiplomStatus());
