293edfa4c7e65b9606f97244e67d1044e5c21fa7
Legion <hersh1409@gmail.com>
2022-08-23 19:43:15 +0500

    auth service deploying

--- src/main/java/second/education/controller/EnrolleeInfoController.java
+++ src/main/java/second/education/controller/EnrolleeInfoController.java
@@ -60,42 +60,42 @@ public class EnrolleeInfoController {
         return ResponseEntity.ok(response);
     }
 
-    @PutMapping("update/foreignDiploma/{diplomaId}")
-    public ResponseEntity<?> updateForeignDiploma(@PathVariable Integer diplomaId,
-                                                  @RequestParam(value = "countryName", required = false) String countryName,
-                                                  @RequestParam(value = "institutionName", required = false) String institutionName,
-                                                  @RequestParam(value = "eduFormName", required = false) String eduFormName,
-                                                  @RequestParam(value = "eduFinishingDate", required = false) String eduFinishingDate,
-                                                  @RequestParam(value = "speciality", required = false) String speciality,
-                                                  @RequestParam(value = "diplomaNumberAndSerial", required = false) String diplomaNumberAndSerial,
-                                                  @RequestParam(value = "diplomaCopyId", required = false) Integer diplomaCopyId,
-                                                  @RequestParam(value = "diploma", required = false) MultipartFile diploma,
-                                                  @RequestParam(value = "diplomaIlovaId", required = false) Integer diplomaIlovaId,
-                                                  @RequestParam(value = "diplomaIlova", required = false) MultipartFile diplomaIlova) {
-        DiplomaResponse response = enrolleeService.updateForeignDiploma(diplomaId, countryName,
-                institutionName, eduFormName, eduFinishingDate, speciality,
-                diplomaNumberAndSerial, diplomaCopyId, diploma, diplomaIlovaId, diplomaIlova);
-        return ResponseEntity.ok(response);
-    }
+//    @PutMapping("update/foreignDiploma/{diplomaId}")
+//    public ResponseEntity<?> updateForeignDiploma(@PathVariable Integer diplomaId,
+//                                                  @RequestParam(value = "countryName", required = false) String countryName,
+//                                                  @RequestParam(value = "institutionName", required = false) String institutionName,
+//                                                  @RequestParam(value = "eduFormName", required = false) String eduFormName,
+//                                                  @RequestParam(value = "eduFinishingDate", required = false) String eduFinishingDate,
+//                                                  @RequestParam(value = "speciality", required = false) String speciality,
+//                                                  @RequestParam(value = "diplomaNumberAndSerial", required = false) String diplomaNumberAndSerial,
+//                                                  @RequestParam(value = "diplomaCopyId", required = false) Integer diplomaCopyId,
+//                                                  @RequestParam(value = "diploma", required = false) MultipartFile diploma,
+//                                                  @RequestParam(value = "diplomaIlovaId", required = false) Integer diplomaIlovaId,
+//                                                  @RequestParam(value = "diplomaIlova", required = false) MultipartFile diplomaIlova) {
+//        DiplomaResponse response = enrolleeService.updateForeignDiploma(diplomaId, countryName,
+//                institutionName, eduFormName, eduFinishingDate, speciality,
+//                diplomaNumberAndSerial, diplomaCopyId, diploma, diplomaIlovaId, diplomaIlova);
+//        return ResponseEntity.ok(response);
+//    }
 
-    @PutMapping("{diplomaId}")
-    public ResponseEntity<?> updateDiploma(Principal principal,
-                                           @PathVariable int diplomaId,
-                                           @RequestParam(value = "countryName", required = false) String countryName,
-                                           @RequestParam(value = "institutionId", required = false) Integer institutionId,
-                                           @RequestParam(value = "id", required = false) Integer id,
-                                           @RequestParam(value = "eduFormName", required = false) String eduFormName,
-                                           @RequestParam(value = "eduFinishingDate", required = false) String eduFinishingDate,
-                                           @RequestParam(value = "speciality", required = false) String speciality,
-                                           @RequestParam(value = "diplomaNumberAndSerial", required = false) String diplomaNumberAndSerial,
-                                           @RequestParam(value = "diplomaCopyId", required = false) Integer diplomaCopyId,
-                                           @RequestParam(value = "diploma", required = false) MultipartFile diploma,
-                                           @RequestParam(value = "diplomaIlovaId", required = false) Integer diplomaIlovaId,
-                                           @RequestParam(value = "diplomaIlova", required = false) MultipartFile diplomaIlova) {
-        Result result = enrolleeService.updateDiploma(principal, diplomaId, countryName, institutionId, id, eduFormName, eduFinishingDate,
-                speciality, diplomaNumberAndSerial, diplomaCopyId, diploma, diplomaIlovaId, diplomaIlova);
-        return ResponseEntity.status(result.isSuccess() ? 200 : 400).body(result);
-    }
+//    @PutMapping("{diplomaId}")
+//    public ResponseEntity<?> updateDiploma(Principal principal,
+//                                           @PathVariable int diplomaId,
+//                                           @RequestParam(value = "countryName", required = false) String countryName,
+//                                           @RequestParam(value = "institutionId", required = false) Integer institutionId,
+//                                           @RequestParam(value = "id", required = false) Integer id,
+//                                           @RequestParam(value = "eduFormName", required = false) String eduFormName,
+//                                           @RequestParam(value = "eduFinishingDate", required = false) String eduFinishingDate,
+//                                           @RequestParam(value = "speciality", required = false) String speciality,
+//                                           @RequestParam(value = "diplomaNumberAndSerial", required = false) String diplomaNumberAndSerial,
+//                                           @RequestParam(value = "diplomaCopyId", required = false) Integer diplomaCopyId,
+//                                           @RequestParam(value = "diploma", required = false) MultipartFile diploma,
+//                                           @RequestParam(value = "diplomaIlovaId", required = false) Integer diplomaIlovaId,
+//                                           @RequestParam(value = "diplomaIlova", required = false) MultipartFile diplomaIlova) {
+//        Result result = enrolleeService.updateDiploma(principal, diplomaId, countryName, institutionId, id, eduFormName, eduFinishingDate,
+//                speciality, diplomaNumberAndSerial, diplomaCopyId, diploma, diplomaIlovaId, diplomaIlova);
+//        return ResponseEntity.status(result.isSuccess() ? 200 : 400).body(result);
+//    }
 
     @PatchMapping("check/diplomas/{diplomaId}")
     public ResponseEntity<?> checkDiploma(Principal principal, @PathVariable int diplomaId) {
